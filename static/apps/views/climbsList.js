e01f6b52bb56312e5f996cc2616f40afca8dec6a
Aaron White <wyrmwood@gmail.com>
2011-06-30 15:28:55 -0400

    setup branch

--- static/apps/views/climbsList.js
+++ static/apps/views/climbsList.js
@@ -3,42 +3,9 @@ Ext.reg("ClimbsIndex", Ext.extend(Ext.Panel, {
    dockedItems: [
       {
         xtype: 'toolbar',
-        title: 'Climbs',
-        items: [
-          { 
-            xtype: 'spacer' 
-          },
-          {
-            xtype: 'button',
-            text: 'New',
-            listeners: {
-              tap: function() {
-                Ext.dispatch({
-                  controller: 'climbs',
-                  action: 'newClimb',
-                  historyUrl: 'climbs/newClimb',
-                  animation: {type:'slide', direction:'up'}                  
-                });
-              }
-            }
-          }          
-        ]
+        title: 'Climbs',        
       }
    ],
-     
-   items:[
-      {
-        xtype: 'list',
-        store: 'AllClimbs',        
-        itemTpl: '{difficulty} - {date}',
-        onItemDisclosure: function(record) {
-          Ext.dispatch({
-            controller: 'climbs',
-            action: 'view',
-            historyUrl: 'climbs/view/' + record.data.id,
-            record: record
-          });
-        }   
-      }
-   ]
+   
+    html: '<div class="welcome">Nice, you made it!</div>'
 }));
